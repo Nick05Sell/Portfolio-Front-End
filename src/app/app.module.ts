c78e9a6ef9281686602ee6e3a9f81cc6b1fc48db
Sell Nicolas <nicolassell81@gmail.com>
2022-12-13 11:52:15 -0300

    Modularizacion del contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuNavegacionComponent } from './componentes/menu-navegacion/menu-navegacion.component';
+import { InformacionComponent } from './componentes/informacion/informacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavegacionComponent,
+    InformacionComponent,
+    ProyectosComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
